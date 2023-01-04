00b0ebab1511688b46a2cdb32c5429a0b658d95a
Valeriy Chernobrovyi <valerasergeevich@gmail.com>
2023-01-04 23:24:42 +0300

    edit test file. 2023/01/04

--- selenium_script_g3.2_step_13.py
+++ selenium_script_g3.2_step_13.py
@@ -1,48 +1,82 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import unittest
 import time
 
-# Файл к уроку 1.6 - Курс "Автоматизация тестирования с помощью Selenium и Python"
 
-try:
-    # It's work in Chrome with Windows, Linux and macOS
-    options = webdriver.ChromeOptions();
-    options.add_argument("--start-maximized");
+# Файл к уроку 3.2, шаг 13 - Курс "Автоматизация тестирования с помощью Selenium и Python"
+class TestAbs(unittest.TestCase):
+    def test_abs1(self):
+        # It's work in Chrome with Windows, Linux and macOS
+        options = webdriver.ChromeOptions();
+        options.add_argument("--start-maximized");
 
-    browser = webdriver.Chrome();
-    browser = webdriver.Chrome(chrome_options=options);
+        browser = webdriver.Chrome();
+        browser = webdriver.Chrome(chrome_options=options);
     
-    link = "http://suninjuly.github.io/registration2.html";
-    browser.get(link);
+        link = "http://suninjuly.github.io/registration1.html";
+        browser.get(link);
 
-    element_1 = browser.find_element(By.CLASS_NAME, 'form-control first');
-    element_1.send_keys("Valeriy");
-    element_2 = browser.find_element(By.CLASS_NAME, 'form-control second');
-    element_2.send_keys("Chernobrovyi");
-    element_3 = browser.find_element(By.CLASS_NAME, 'form-control third');
-    element_3.send_keys("valerasergeevich@gmail.com");
+        element_1 = browser.find_element(By.CLASS_NAME, 'form-control first');
+        element_1.send_keys("Valeriy");
+        element_2 = browser.find_element(By.CLASS_NAME, 'form-control second');
+        element_2.send_keys("Chernobrovyi");
+        element_3 = browser.find_element(By.CLASS_NAME, 'form-control third');
+        element_3.send_keys("valerasergeevich@gmail.com");
 
 
-    # Отправляем заполненную форму
-    button = browser.find_element(By.CSS_SELECTOR, "button.btn")
-    button.click()
+        # Отправляем заполненную форму
+        button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+        button.click()
 
-    # Проверяем, что смогли зарегистрироваться
-    # ждем загрузки страницы
-    time.sleep(1)
+        # Проверяем, что смогли зарегистрироваться
+        # ждем загрузки страницы
+        time.sleep(1)
 
-    # находим элемент, содержащий текст
-    welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
-    # записываем в переменную welcome_text текст из элемента welcome_text_elt
-    welcome_text = welcome_text_elt.text
+        # находим элемент, содержащий текст
+        welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+        # записываем в переменную welcome_text текст из элемента welcome_text_elt
+        welcome_text = welcome_text_elt.text
 
-    # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
-    assert "Congratulations! You have successfully registered!" == welcome_text
+        # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+        assert "Congratulations! You have successfully registered!" == welcome_text;
 
-finally:
-    # успеваем скопировать код за 30 секунд
-    time.sleep(30);
-    # закрываем браузер после всех манипуляций
-    browser.quit();
+    def test_abs2(self):
+        # It's work in Chrome with Windows, Linux and macOS
+        options = webdriver.ChromeOptions();
+        options.add_argument("--start-maximized");
+
+        browser = webdriver.Chrome();
+        browser = webdriver.Chrome(chrome_options=options);
+    
+        link = "http://suninjuly.github.io/registration2.html";
+        browser.get(link);
+
+        element_1 = browser.find_element(By.CLASS_NAME, 'form-control first');
+        element_1.send_keys("Valeriy");
+        element_2 = browser.find_element(By.CLASS_NAME, 'form-control second');
+        element_2.send_keys("Chernobrovyi");
+        element_3 = browser.find_element(By.CLASS_NAME, 'form-control third');
+        element_3.send_keys("valerasergeevich@gmail.com");
+
+
+        # Отправляем заполненную форму
+        button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+        button.click()
+
+        # Проверяем, что смогли зарегистрироваться
+        # ждем загрузки страницы
+        time.sleep(1)
+
+        # находим элемент, содержащий текст
+        welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+        # записываем в переменную welcome_text текст из элемента welcome_text_elt
+        welcome_text = welcome_text_elt.text
+
+        # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+        assert "Congratulations! You have successfully registered!" == welcome_text;
+
+if __name__ == "__main__":
+    unittest.main();
 
 # не забываем оставить пустую строку в конце файла
